33da2b63a29e80b7372ed19606db7c03d1f02aed
SNomi1123 <tbofbfqs90@gmail.com>
2023-10-23 19:26:05 +0800

    Revert "修改對話框程式"
    This reverts commit 4bd4d6a8d7a1d52b77f485ee67330da306e1d863.

--- Project/Assets/Script/CursorControl.cs
+++ Project/Assets/Script/CursorControl.cs
@@ -7,30 +7,20 @@ public class CursorControl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
 
 
         if (Input.GetKeyDown("2"))
         {
-
-            Cursor.visible = true;
-        }
-
-        if (RayScript.isTalking == true)
-        {
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
-        }
-        else
-        {
-            Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = false;
         }
         
     }
